92bfcee6dcac05aab74e9e138264d08b80ec471b
sencha930511 <sencha930511@gmail.com>
2024-09-16 15:40:41 +0800

    fix some bugs

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from ntcu_api import Ntcu_api
 from bs4 import BeautifulSoup
 
-ntcu_api = Ntcu_api("ACS111132", "tim-930511")
+ntcu_api = Ntcu_api("ACS111132", "aaaa")
 
 r = ntcu_api.getAllCourses()
 
